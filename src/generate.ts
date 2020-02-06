afd3269b3dd41089980a31f33cbcb1e71073ee56
Marek Lukáš <tajnymag@gmail.com>
2020-02-06 11:53:38 +0100

    Base files

---
+++ src/generate.ts
@@ -0,0 +1,2 @@
+import {generateTemplateClassesFromXSD} from "xsd2ts";
+generateTemplateClassesFromXSD('./isdoc-invoice-6.0.1.xsd');
---
+++ src/generated/generate.ts
@@ -0,0 +1,8 @@
+/***********
+generated template classes for ./src/generate.ts 6. 2. 2020 11:37:00
+***********/
+
+
+
+
+
